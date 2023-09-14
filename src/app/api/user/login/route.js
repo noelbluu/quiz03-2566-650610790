c988f201a47442680aa78cdae481ae0b4e748b39
noelbluu <panida.stp@icloud.com>
2023-09-14 14:15:47 +0700

    update user login

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -6,15 +6,31 @@ import { NextResponse } from "next/server";
 export const POST = async (request) => {
   readDB();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or Password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
+  const body = await request.json()
+  const { username, password } = body
 
-  const token = "Replace this with token creation";
+  const user = DB.users.find(
+    (user) => user.username === username && user.password === password
+  )
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Username or Password is incorrect",
+      },
+      { status: 400 }
+    );
+  }
+
+  const token = jwt.sign(
+    {
+      username,
+      role: user.role
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "8h" }
+  )
 
   return NextResponse.json({ ok: true, token });
 };
